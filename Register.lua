2ed5a708a5e60aacd6900363cde134364c594ff8
r_ <94858656+fayvrit@users.noreply.github.com>
2025-03-02 00:45:28 +0800

    Update Register.lua

--- Register.lua
+++ Register.lua
@@ -1,124 +1,115 @@
-local Typeface = {  }
+local Library = {  }
+
+-- // Assert Alternative
+Library.Throw = function(Condition, String)
+    return Condition or error(String)
+end
 
 --[[ Compatibility Check ]] do
     cloneref                = cloneref or function(service) return service end
 
-    isfile                  = assert(isfile, "Executor is incompatible!")
-    isfolder                = assert(isfolder, "Executor is incompatible!")
-    writefile               = assert(writefile, "Executor is incompatible!")
-    makefolder              = assert(makefolder, "Executor is incompatible!")
-    getcustomasset 			= assert(getcustomasset, "Executor is incompatible!")
+    isfile                  = Library.Throw(isfile, 'Executor is incompatible!')
+    isfolder                = Library.Throw(isfolder, 'Executor is incompatible!')
+    writefile               = Library.Throw(writefile, 'Executor is incompatible!')
+    makefolder              = Library.Throw(makefolder, 'Executor is incompatible!')
+    getcustomasset          = Library.Throw(getcustomasset, 'Executor is incompatible!')
+end
+
+-- // Metatables 
+Library.__index = function(self, key)
+    local lowerkey = string.lower(key)
+
+    return 
+        rawget(self, lowerkey) or 
+        rawget(self, key) or 
+        rawget(Library, lowerkey) or 
+        rawget(Library, key)
 end
 
 -- // Services
 local Http = cloneref(game:GetService 'HttpService')
 
 -- // Tables
-Typeface.Typefaces = {  }
-Typeface.WeightNum = { 
-	["Thin"] = 100,
-
-	["ExtraLight"] = 200, 
-	["UltraLight"] = 200,
-
-	["Light"] = 300,
-
-	["Normal"] = 400,
-	["Regular"] = 400,
-
-	["Medium"] = 500,
-
-	["SemiBold"] = 600,
-	["DemiBold"] = 600,
-
-	["Bold"] = 700,
-
-	["ExtraBold"] = 800,
-
-	["UltraBold"] = 900,
-	["Heavy"] = 900
-}
+Library.Fonts = { }
 
 -- // Functions
-function Typeface:RequestFile(URL)
-	local Response = request{Url = URL, Method = "GET"}
+function Library:Register(Info)
+    Info = setmetatable(Info or { }, Library)
 
-	assert(Response.StatusCode == 200, `[ Typeface Registration ] Content Error! : { Response.StatusCode }`)
+    Info.url = Library.Throw(Info.link, 'Link is missing!')
+    Info.name = Library.Throw(Info.name, 'Name is missing!')
+    Info.id = Library.Throw(Info.id, 'ID is missing!')
 
-    return Response.Body
+    Info.weight = Info.weight or 'Regular'
+    Info.style = Info.style or 'Normal'
+
+    Info.path = Library.Throw(Info.path or Library.Path, 'Path is missing!')
+    Info.enumweight = Library.Throw(Enum.FontWeight[Info.weight], `Couldn't find enum weight for "{Info.weight}"!`)
+    Info.numweight = Library.Throw(Info.enumweight.Value, `Couldn't find numerical weight for "{Info.weight}"!`)
+
+    Info.fullname = `{ Info.numweight ~= '400' and '-' .. Info.weight or '' }{ Info.style ~= 'Normal' and '-' .. Info.style or '' }`
+    Info.fontpath = `{ Info.path }\\{ Info.fullname }.font`
+    Info.familypath = `{ Info.path }\\Families`
+
+    Info.family = self.family
+
+    Library:CheckPath(Info.path)
+    Library:CheckPath(Info.familypath)
+
+    Info:CreateFont()
+    Info:CreateFamily()
+
+    Library.Fonts[Info.fullname] = Info
+    Library.Path = Info.path
+
+    return Info:Get(), Info
 end
 
-function Typeface:Writeface(Directory, File)
-    File = Http:JSONEncode(File)
+function Library:Get(Name)
+    local face = Library.Fonts[Name] or self
 
-
-    writefile(Directory, File)
+    return Font.new(face.font, face.enumweight)
 end
 
-function Typeface:Register(Path, Asset)
-	Asset = Asset or {}
+function Library:CheckPath(Path)
+    if isfolder(Path) then return end
 
-    Asset.weight = Asset.weight or "Regular"
-    Asset.style = Asset.style or "Normal"
-
-    assert(Asset.link, `[ Typeface Registration ] "link" is required to Register a Typeface!`)
-    assert(Asset.name, `[ Typeface Registration ] "name" is required to Register a Typeface!`)
-
-	local Directory = `{ Path or "" }\\{ Asset.name }`
-
-    local Weight = Typeface.WeightNum[Asset.weight] == 400 and "" or Asset.weight
-    local Style = string.lower(Asset.style) == "normal" and "" or Asset.style
-	local Name = `{ Asset.name }{ Weight }{ Style }`
-
-    local Registered = false
-    local JSONFile, Data
-
-    if not isfolder(Directory) then
-        makefolder(Directory)
-    end
-
-    if not isfile(`{ Directory }\\{ Name }.font`) then
-        local Response = Typeface:RequestFile(Asset.link)
-        
-        if not Response then return end
-
-        writefile(`{ Directory }\\{ Name }.font`, Response)
-	end
-
-    if isfile(`{ Directory }\\{ Asset.name }Families.json`) then 
-        Data = { 
-            name = `{ Asset.weight } { Asset.style }`,
-            weight = Typeface.WeightNum[Asset.weight] or Typeface.WeightNum[string.gsub(Asset.weight, "%s+", "")],
-            style = string.lower(Asset.style),
-            assetId = getcustomasset(`{ Directory }\\{ Name }.font`)
-		}
-
-        JSONFile = Http:JSONDecode(readfile(`{ Directory }\\{ Asset.name }Families.json`))
-
-        for _, face in JSONFile.faces do
-            if face.name == Data.name then Registered = true break end
-        end
-
-        if not Registered then
-            table.insert(JSONFile.faces, Data)
-
-            Typeface:Writeface(`{ Directory }\\{ Asset.name }Families.json`, JSONFile)
-            warn(`[ Typeface Registration ] Registering { Asset.weight } { Asset.style } Typeface to "{ Directory }"...`)
-        end
-	else
-		Data = { 
-			name = `{ Asset.weight } { Asset.style }`,
-			weight = Typeface.WeightNum[Asset.weight] or Typeface.WeightNum[string.gsub(Asset.weight, "%s+", "")],
-			style = string.lower(Asset.style),
-			assetId = getcustomasset(`{ Directory }\\{ Name }.font`)
-		}
-
-		Typeface:Writeface(`{ Directory }\\{ Asset.name }Families.json`, { name = Name, faces = { Data } })
-        warn(`[ Typeface Registration ] Registering { Asset.name } Typeface to "{ Path }"...`)
-	end
-
-	Typeface.Typefaces[Name] = Typeface.Typefaces[Name] or Font.new(getcustomasset(`{ Directory }\\{ Asset.name }Families.json`))
-    return Typeface.Typefaces[Name]
+    makefoler(Path)
 end
 
-return Typeface
+function Library:CreateFont()
+    local Info = Library.Throw(self.Info, 'Information is missing!')
+
+    if isfile(Info.fontpath) then return end
+
+    local Success, Result = pcall(function()
+        return game:HttpGet(Info.url)
+    end)
+
+    Library.Throw(Success, `Failed to fetch "{Info.name .. Info.weight .. Info.style}" file!`)
+    warn(`Successfully fetched "{Info.name .. Info.weight .. Info.style}" file!`)
+
+    writefile(Info.fontpath, Result)
+
+    Info.fontpath = getcustomasset(Info.fontpath)
+end
+
+function Library:CreateFamily()
+    local Info = Library.Throw(self.Info, 'Information is missing!')
+
+    Info.family = Info.family or {
+        name = Info.name,
+        faces = { }
+    }
+
+    table.insert({
+        name = string.gsub(Info.fullname, '-', ' '),
+        weight = Info.numweight,
+        style = Info.style,
+        assetId = getcustomasset(Info.fontpath),
+    }, Info.family.faces)
+
+    writefile(`{ Info.familypath }\\{ Info.name }.json`, Http:JSONEncode(Info.family))
+end
+
+return Library
